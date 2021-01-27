a57a0f40618c8ec74be63855482f1970ab56c37f
Huriel Lopes <huriel-lopes@hotmail.com>
2021-01-26 22:48:15 -0300

    Criação da estrutura inicial para começar a trabalhar com hooks

---
+++ reacthooks/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+
+import { Container, Typography } from "@material-ui/core";
+
+function App() {
+  return (
+    <Container component="article" maxWidth="sm">
+      <Typography variant="h3" align="center" component="h1">Formulário de cadastro</Typography>
+      <FormularioCadastro />
+    </Container>
+  );
+}
+
+export default App;
